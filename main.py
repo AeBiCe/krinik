c56b8348f306e5d9a66c2865efe2e7d5b446a54d
koffe <kb222wa@student.lnu.se>
2020-12-19 16:15:16 +0100

    Register customer basic implementation complete

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-createTables = True
+createTables = False
 #First draft of create tables.
 if createTables:
     mycursor.execute("CREATE TABLE ORDERS (o_id VARCHAR(255), c_id VARCHAR(255), g_id VARCHAR(255), total_price VARCHAR(255))")
@@ -22,24 +22,39 @@ if createTables:
     mycursor.execute("CREATE TABLE GAME (g_id VARCHAR(255), price VARCHAR(255), g_name VARCHAR(255))")
 
 
-# print("======MENU======")
-# print("1.Register Customer\n2.Change customer info*TEMP*\n3.New order\n4.View Specific Order\n"
-#       "5.Show Store Statistics\n0.Quit")
-#
-# choice = int(input("Enter menu choice: "))
-# if choice == 1:
-#     print("First choice")
-# elif choice == 2:
-#     print("Second choice")
-# elif choice == 3:
-#     print("Third choice")
-# elif choice == 4:
-#     print("Fourth choice")
-# elif choice == 5:
-#     print("Fifth choice")
-# elif choice == 0:
-#     print("Game over.")
-# else:
-#     print("Choice not an option.")
+
+
+
+print("======MENU======")
+print("1.Register Customer\n2.Change customer info*TEMP*\n3.New order\n4.View Specific Order\n"
+      "5.Show Store Statistics\n0.Quit")
+
+
+def register_customer():
+    c_name = str(input("Enter Customer name: "))
+    gender = str(input("Enter Customer gender: "))
+    c_id = int(input("Enter Customer ID: "))
+    sql = "INSERT INTO CUSTOMER (c_id, c_name, gender) VALUES (%s,%s,%s)"
+    val = (c_id, c_name, gender)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+
+
+choice = int(input("Enter menu choice: "))
+if choice == 1:
+    register_customer()
+elif choice == 2:
+    print("Second choice")
+elif choice == 3:
+    print("Third choice")
+elif choice == 4:
+    print("Fourth choice")
+elif choice == 5:
+    print("Fifth choice")
+elif choice == 0:
+    print("Game over.")
+else:
+    print("Choice not an option.")
 
 
