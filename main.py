f0fdee998faeb4f0d46dbc8adb74d4937466e2df
koffe <kb222wa@student.lnu.se>
2020-12-19 15:26:22 +0100

    Create tables added. Temporary column values.

--- main.py
+++ main.py
@@ -11,26 +11,35 @@ mydb = mysql.connector.connect(
     password="oldplayer11",
     database="krinik"
 )
-print("======MENU======")
-print("1.Register Customer\n2.Change customer info*TEMP*\n3.New order\n4.View Specific Order\n"
-      "5.Show Store Statistics\n0.Quit")
+
+mycursor = mydb.cursor()
+
+createTables = True
+#First draft of create tables.
+if createTables:
+    mycursor.execute("CREATE TABLE ORDERS (o_id VARCHAR(255), cus_id VARCHAR(255), game_id VARCHAR(255), total_price VARCHAR(255))")
+    mycursor.execute("CREATE TABLE CUSTOMER (c_id VARCHAR(255), c_name VARCHAR(255), gender VARCHAR(255))")
+    mycursor.execute("CREATE TABLE GAME (g_id VARCHAR(255), price VARCHAR(255), g_name VARCHAR(255))")
 
 
-
-choice = int(input("Enter menu choice: "))
-if choice == 1:
-    print("First choice")
-elif choice == 2:
-    print("Second choice")
-elif choice == 3:
-    print("Third choice")
-elif choice == 4:
-    print("Fourth choice")
-elif choice == 5:
-    print("Fifth choice")
-elif choice == 0:
-    print("Game over.")
-else:
-    print("Choice not an option.")
+# print("======MENU======")
+# print("1.Register Customer\n2.Change customer info*TEMP*\n3.New order\n4.View Specific Order\n"
+#       "5.Show Store Statistics\n0.Quit")
+#
+# choice = int(input("Enter menu choice: "))
+# if choice == 1:
+#     print("First choice")
+# elif choice == 2:
+#     print("Second choice")
+# elif choice == 3:
+#     print("Third choice")
+# elif choice == 4:
+#     print("Fourth choice")
+# elif choice == 5:
+#     print("Fifth choice")
+# elif choice == 0:
+#     print("Game over.")
+# else:
+#     print("Choice not an option.")
 
 
