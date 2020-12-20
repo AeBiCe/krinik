3f87f184f423ac8ae75b59012ad7409340e2e1b7
koffe <kb222wa@student.lnu.se>
2020-12-20 07:58:16 +0100

    basic place_order complete.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def register_customer():
 def create_game():
     price = int(input("Enter price of the new game: "))
     g_name = str(input("Enter the name of the new game: "))
-    sql = "INSERT INTO GAME (price, g_name) VALUES (%s,%s)"
+    sql = "INSERT IGNORE INTO GAME (price, g_name) VALUES (%s,%s)"
     val = (price, g_name)
     mycursor.execute(sql, val)
     mydb.commit()
@@ -51,14 +51,14 @@ def create_game():
 
 def place_order():
     # take customer id and game id from consol
-    c_id = int(input("Enter Customer ID: "))
-    g_id = int(input("Enter game ID: "))
+    c_id = input("Enter Customer ID: ")
+    g_id = input("Enter game ID: ")
     sql_get_price = ("select price from krinik.game where g_id = %s" % g_id)  # Query to select price from game id entered
     total_price = mycursor.execute(sql_get_price, g_id)  # Execute query and use game id entered as value
-    tot_price = mycursor.fetchone() # Fetches value from cursor
+    total_price = mycursor.fetchone()  # Fetches value from cursor
     #print("PRICE: ",tot_price)  # Error handling
-    final_price = int(tot_price[0]) # take first value since tot_price is a list
-    sql = "INSERT INTO ORDER(c_id, g_id, total_price) VALUES (%s,%s,%s)"  # SQL query to insert order into the table
+    final_price = total_price[0]  # take first value since tot_price is a list
+    sql = "INSERT IGNORE INTO ORDERS(c_id, g_id, total_price) VALUES (%s,%s,%s)"  # SQL query to insert order into the table
     val = (c_id, g_id, final_price)
     mycursor.execute(sql, val)
     mydb.commit()
