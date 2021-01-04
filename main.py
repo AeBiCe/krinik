66068174f17baf4ed4eaada61238d84f4012c8e8
Koffe <koffe296@hotmail.com>
2021-01-04 19:13:05 +0100

    Query changes. Can add multiple games in one order fixed.

--- main.py
+++ main.py
@@ -63,40 +63,46 @@ def fill_order(c_id, g_id):
     total_price = mycursor.fetchone()  # Fetches value from cursor
     #print("PRICE: ",tot_price)  # Error handling
     final_price = total_price[0]  # take first value since tot_price is a list
-    order_num = str(c_id) + str(id(g_id))
     sql = "INSERT IGNORE INTO ORDERS(c_id, total_price) VALUES (%s,%s)"  # SQL query to insert order into the table
-    val = (c_id, g_id, order_num, final_price)
+    val = (c_id, final_price)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    sql = "SELECT MAX(o_id) FROM krinik.orders"
+    mycursor.execute(sql)
+    o_id = mycursor.fetchone()
+    sql = "INSERT IGNORE INTO ORDERINFO(o_id,game) VALUES(%s,%s)"
+    val = (o_id[0], g_id)
     mycursor.execute(sql, val)
     mydb.commit()
 # ==============================================================================================================
 
 # Variables to set to true/false depending on what is desired
-createTables = True  # set to true if you want to create new empty tables
-menuOptions = False  # set to true if you want to activate menu to check the database
+createTables = False  # set to true if you want to create new empty tables
+menuOptions = True  # set to true if you want to activate menu to check the database
 fillTables = False  # set to true if you want to fill the tables with customer, games and orders
 
 if createTables:
-    mycursor.execute("DROP TABLE IF EXISTS Orders")
-    mycursor.execute("DROP TABLE IF EXISTS Customer")
     mycursor.execute("DROP TABLE IF EXISTS Game")
     mycursor.execute("DROP TABLE IF EXISTS ORDERINFO")
+    mycursor.execute("DROP TABLE IF EXISTS Orders")
+    mycursor.execute("DROP TABLE IF EXISTS Customer")
 
     mycursor.execute("use krinik;")
     mycursor.execute("CREATE TABLE CUSTOMER (c_id INT AUTO_INCREMENT PRIMARY KEY, c_name VARCHAR(255) NOT NULL, "
                      "gender VARCHAR(255) NOT NULL);")
     mycursor.execute("CREATE TABLE ORDERS(o_id INT AUTO_INCREMENT PRIMARY KEY, total_price INT NOT NULL,"
-                     "c_id INT NOT NULL, FOREIGN KEY(c_id) REFERENCES CUSTOMER(c_id)")
+                     "c_id INT NOT NULL, FOREIGN KEY(c_id) REFERENCES CUSTOMER(c_id))")
     mycursor.execute("CREATE TABLE GAME (g_id INT AUTO_INCREMENT PRIMARY KEY, price INT NOT NULL,"
                      " g_name VARCHAR(255) NOT NULL);")
-    mycursor.execute("CREATE TABLE ORDERINFO (info_id INT AUTO_INCREMENT PRIMARY KEY, "
+    mycursor.execute("CREATE TABLE ORDERINFO (info_id INT AUTO_INCREMENT PRIMARY KEY,o_id INT NOT NULL, "
                      "FOREIGN KEY(o_id) REFERENCES ORDERS(o_id)"
                      ", game VARCHAR(255) NOT NULL)")
 
 
 if fillTables:
     fill_customers()
-    # fill_games()
-    for x in range(100):
+    fill_games()
+    for x in range(500):
         c_id = random.randint(1,tot_customer)
         g_id = random.randint(1,tot_games)
         fill_order(c_id,g_id)
@@ -149,6 +155,15 @@ def place_order():
     mycursor.execute(sql, val)
     mydb.commit()
 
+    sql = "SELECT MAX(o_id) FROM krinik.orders"
+    mycursor.execute(sql)
+    o_id = mycursor.fetchone()
+    sql = "INSERT IGNORE INTO ORDERINFO(o_id,game) VALUES(%s,%s)"
+    for game in basket:
+        val = (o_id[0], int(game))
+        mycursor.execute(sql, val)
+        mydb.commit()
+
     if len(basket) == 0:
         print("No order was placed, basket was empty!") # If basket was emty at checkout the user is notified
     else:
@@ -159,18 +174,24 @@ def place_order():
 
 
 def view_specific_order():
+    sql = "drop view if exists price;"
+    mycursor.execute(sql)
+    sql = " create view price as select price, g_id from game;"
+    mycursor.execute(sql)
     o_id = input("Enter order ID: ")
-    sql = ("select * from krinik.orders where o_id = %s" % o_id)  # Query to select specific order
+    sql = ("SELECT o.o_id, p.price, o.c_id, oi.game from orders o inner join orderinfo as oi on oi.o_id = o.o_id join "
+           "price as p on oi.game = p.g_id where o.o_id = %s" % o_id)  # Query to select specific order
     mycursor.execute(sql, o_id)
-    result = mycursor.fetchone()  # Fetch the result into a list
-    print("Order ID:",result[0],", Customer ID:",result[1],", Game ID:",result[2],", Order price:",result[3],"$")
+    result = mycursor.fetchall()  # Fetch the result into a list
+    for order in result:
+        print("Order ID:",order[0], "| Order price:$",order[1], "| Customer ID: ",order[2], "| Game ID: ",order[3])
 
 
 def most_sold_game():
     mycursor.execute("DROP VIEW IF EXISTS topFiveGames")  # if view exists then drop it to create new.
     # We create a view and left join it with table game to only get games that are sold.
-    sql = "CREATE VIEW topFiveGames as select ord.g_id, gm.g_name from orders as ord " \
-          "left join game as gm on ord.g_id = gm.g_id"
+    sql = "CREATE VIEW topFiveGames as select ord.game, gm.g_name from krinik.orderinfo as ord " \
+          "left join game as gm on ord.game = gm.g_id;"
     mycursor.execute(sql)  # execute first query to create view
     # We select the name of the game and the copies sold of that game from the view that we created.
     sql2 = "Select g_name, COUNT(g_name) as copies from topFiveGames GROUP BY g_name ORDER BY copies DESC LIMIT 5"
@@ -204,26 +225,32 @@ def loyal_customer():
     sql3 = "select cus_name, MAX(the_sum) as top from customersum group by the_sum order by the_sum DESC LIMIT 1"
     mycursor.execute(sql3) # execute query above
     most_loyal = mycursor.fetchone()
-    print("Customer name: ",most_loyal[0],", Total amount spent: ",most_loyal[1],"$")
+    print("Customer name: ",most_loyal[0],", Total amount spent: $",most_loyal[1])
 
 
 def popular_gender_game():
-    mycursor.execute("drop view if exists genderStat")
+    mycursor.execute("drop view if exists firstView;drop view if exists getCustomer;drop view if exists genderStat;")
     # create view with game id and gender and join with customer table
-    sql = "CREATE view genderStat as SELECT o.g_id, c.gender from orders as o right join customer " \
-          "as c on o.c_id = c.c_id"
+    sql = "create view firstView as SELECT oi.game, o.o_id from orderinfo as oi " \
+          "left join orders as o on o.o_id = oi.o_id;"
     mycursor.execute(sql)
-    mycursor.execute("drop view if exists popGender")
+    sql = "create view getCustomer as SELECT o.c_id, fv.game from orders as o " \
+          "left join firstView as fv on o.o_id = fv.o_id;"
+    mycursor.execute(sql)
+    sql = "CREATE view genderStat as SELECT o.game, c.gender from getcustomer as o " \
+          "right join customer as c on o.c_id = c.c_id;"
+    mycursor.execute(sql)
+
     gender = input("What gender do you want to look up (male or female): ")
     # create view and select game id and the amount of times it occurs
     # to find the most sold game where the gender matches
-    sql2 = ("create view popGender as select g_id, count(g_id) as times from genderStat where BINARY gender = '%s' " \
-           "group by g_id order by times DESC LIMIT 5" % gender)
-    mycursor.execute(sql2, gender)
+    sql = ("create view popGender as select game, count(game) as times from genderStat where BINARY gender = '%s'" \
+          " group by game order by times DESC LIMIT 5;" %gender)
+    mycursor.execute(sql, gender)
     # We select the game name and the copies sold of that game from previous table
-    sql3 = "SELECT times, g_name from popGender as p right join game as g on p.g_id = g.g_id " \
-           "group by g_name order by times DESC LIMIT 1"
-    mycursor.execute(sql3)
+    sql = "SELECT times, g_name from popGender as p right join game as g on p.game = g.g_id" \
+           " group by g_name order by times DESC LIMIT 1"
+    mycursor.execute(sql)
     result = mycursor.fetchone()
     print("Game name: ",result[1]," ,Copies sold: ",result[0])
 
